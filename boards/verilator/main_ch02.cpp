510cd019a315035817ff898501c5c9a3d5ba66fa
Will Green <WillGreen@users.noreply.github.com>
2025-08-21 11:24:34 +0100

    Add chapter 2 verilator top

---
+++ boards/verilator/main_ch02.cpp
@@ -0,0 +1,137 @@
+// Isle.Computer - Chapter 2: Verilator C++
+// Copyright Will Green and Isle Contributors
+// SPDX-License-Identifier: MIT
+
+#include <stdio.h>
+#include <SDL.h>
+#include <verilated.h>
+#include "Vtop_ch02.h"
+
+// display dimensions (must match hardware design or you'll core dump)
+const int H_RES = 672, V_RES = 384;
+
+const int FULLSCREEN = false;
+
+typedef struct Pixel {  // for SDL texture
+    uint8_t a;  // transparency
+    uint8_t b;  // blue
+    uint8_t g;  // green
+    uint8_t r;  // red
+} Pixel;
+
+int main(int argc, char* argv[]) {
+    Verilated::commandArgs(argc, argv);
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL init failed.\n");
+        return 1;
+    }
+
+    Pixel screenbuffer[H_RES*V_RES];
+
+    SDL_Window*   sdl_window   = NULL;
+    SDL_Renderer* sdl_renderer = NULL;
+    SDL_Texture*  sdl_texture  = NULL;
+
+    sdl_window = SDL_CreateWindow("Isle - Chapter 2", SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED, H_RES, V_RES, SDL_WINDOW_SHOWN);
+    if (!sdl_window) {
+        printf("Window creation failed: %s\n", SDL_GetError());
+        return 1;
+    }
+    if (FULLSCREEN) SDL_SetWindowFullscreen(sdl_window, SDL_WINDOW_FULLSCREEN_DESKTOP);
+
+    sdl_renderer = SDL_CreateRenderer(sdl_window, -1,
+        SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (!sdl_renderer) {
+        printf("Renderer creation failed: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    int set_render_size_status;
+    set_render_size_status = SDL_RenderSetLogicalSize(sdl_renderer, H_RES, V_RES);
+    if (set_render_size_status != 0) {
+        printf("Renderer size setting failed: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    sdl_texture = SDL_CreateTexture(sdl_renderer, SDL_PIXELFORMAT_RGBA8888,
+        SDL_TEXTUREACCESS_TARGET, H_RES, V_RES);
+    if (!sdl_texture) {
+        printf("Texture creation failed: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    // reference SDL keyboard state array: https://wiki.libsdl.org/SDL_GetKeyboardState
+    const Uint8 *keyb_state = SDL_GetKeyboardState(NULL);
+
+    // enable text input: https://wiki.libsdl.org/SDL2/SDL_StartTextInput
+    SDL_StartTextInput();
+
+    printf("Simulation running. Hold ctrl+esc in simulation window to quit.\n");
+    printf("Please be patient waiting for simulated display output.\n\n");
+
+    // initialize Verilog module
+    Vtop_ch02* top = new Vtop_ch02;
+
+    // reset
+    top->rst = 1;
+    top->clk = 0;
+    top->eval();
+    top->clk = 1;
+    top->eval();
+    top->rst = 0;
+    top->clk = 0;
+    top->eval();
+
+    uint64_t frame_count = 0;
+    uint64_t start_ticks = SDL_GetPerformanceCounter();
+    while (1) {
+        // cycle the clock
+        top->clk = 1;
+        top->eval();
+        top->clk = 0;
+        top->eval();
+
+        // update pixel if not in blanking interval
+        if (top->sdl_de) {
+            Pixel* p = &screenbuffer[top->sdl_y*H_RES + top->sdl_x];
+            p->a = 0xFF;  // transparency
+            p->b = top->sdl_b;
+            p->g = top->sdl_g;
+            p->r = top->sdl_r;
+        }
+
+        // update texture once per frame (in blanking)
+        if (top->sdl_frame) {
+            // check for quit event
+            SDL_Event e;
+            if (SDL_PollEvent(&e)) {
+                if (e.type == SDL_QUIT) {
+                    break;
+                }
+            }
+
+            if (keyb_state[SDL_SCANCODE_ESCAPE] && SDL_GetModState() & KMOD_CTRL) break;  // quit if user presses ctrl+esc
+
+            SDL_UpdateTexture(sdl_texture, NULL, screenbuffer, H_RES*sizeof(Pixel));
+            SDL_RenderClear(sdl_renderer);
+            SDL_RenderCopy(sdl_renderer, sdl_texture, NULL, NULL);
+            SDL_RenderPresent(sdl_renderer);
+            frame_count++;
+        }
+    }
+    uint64_t end_ticks = SDL_GetPerformanceCounter();
+    double duration = ((double)(end_ticks-start_ticks))/SDL_GetPerformanceFrequency();
+    double fps = (double)frame_count/duration;
+    printf("Frames per second: %.1f\n", fps);
+
+    top->final();  // simulation done
+
+    SDL_StopTextInput();
+    SDL_DestroyTexture(sdl_texture);
+    SDL_DestroyRenderer(sdl_renderer);
+    SDL_DestroyWindow(sdl_window);
+    SDL_Quit();
+    return 0;
+}
