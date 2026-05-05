822ec7da8bfdf55f74229ed7bc36a8ad459246f2
Will Green <WillGreen@users.noreply.github.com>
2026-05-05 19:04:31 +0100

    Improve ECP5 DVI Generator (#36)
    - separate ring counter for each TMDS channel for better routing
    - reduce skew by sending clock channel through ODDRX1F
    - add explanatory comments

--- hardware/arch/ecp5/dvi_generator.v
+++ hardware/arch/ecp5/dvi_generator.v
@@ -19,7 +19,7 @@ module dvi_generator (
     output wire ch0_dout,        // channel 0 - serial TMDS
     output wire ch1_dout,        // channel 1 - serial TMDS
     output wire ch2_dout,        // channel 2 - serial TMDS
-    output reg  clk_dout         // clock channel - serial TMDS
+    output wire clk_dout         // clock channel - serial TMDS
     );
 
     wire [9:0] tmds_ch0, tmds_ch1, tmds_ch2;
@@ -51,18 +51,25 @@ module dvi_generator (
         .tmds(tmds_ch2)
     );
 
+    // use a separate ring counter for each channel to reduce fanout
+    reg [4:0] shift5_ch0 = 5'b00001;
+    reg [4:0] shift5_ch1 = 5'b00001;
+    reg [4:0] shift5_ch2 = 5'b00001;
+
     reg [9:0] ch0_shift, ch1_shift, ch2_shift;
-    reg [4:0] shift5 = 1;  // 5-bit circular shift buffer
+
+    // shift 2 bits of TMDS data every cycle; load new TMDS data every 5 cycles
     always @(posedge clk_pix_5x) begin
-        shift5 <= {shift5[3:0], shift5[4]};
-        ch0_shift <= shift5[4] ? tmds_ch0 : ch0_shift >> 2;  // shift two bits for DDR
-        ch1_shift <= shift5[4] ? tmds_ch1 : ch1_shift >> 2;
-        ch2_shift <= shift5[4] ? tmds_ch2 : ch2_shift >> 2;
+        shift5_ch0 <= {shift5_ch0[3:0], shift5_ch0[4]};
+        shift5_ch1 <= {shift5_ch1[3:0], shift5_ch1[4]};
+        shift5_ch2 <= {shift5_ch2[3:0], shift5_ch2[4]};
+        ch0_shift <= shift5_ch0[4] ? tmds_ch0 : ch0_shift >> 2;  // shift two bits for DDR
+        ch1_shift <= shift5_ch1[4] ? tmds_ch1 : ch1_shift >> 2;
+        ch2_shift <= shift5_ch2[4] ? tmds_ch2 : ch2_shift >> 2;
     end
 
     ODDRX1F serialize_ch0 (.D0(ch0_shift[0]), .D1(ch0_shift[1]), .Q(ch0_dout), .SCLK(clk_pix_5x), .RST(1'b0));
     ODDRX1F serialize_ch1 (.D0(ch1_shift[0]), .D1(ch1_shift[1]), .Q(ch1_dout), .SCLK(clk_pix_5x), .RST(1'b0));
     ODDRX1F serialize_ch2 (.D0(ch2_shift[0]), .D1(ch2_shift[1]), .Q(ch2_dout), .SCLK(clk_pix_5x), .RST(1'b0));
-
-    always @(*) clk_dout = clk_pix;  // clock isn't following same path as other channels
+    ODDRX1F serialize_clk (.D0(1'b1),         .D1(1'b0),         .Q(clk_dout), .SCLK(clk_pix),    .RST(1'b0));
 endmodule
